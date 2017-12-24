d5383716699aa6ca0f6d2caada465c0bda3fd9ce
Yining Chen <yining.chen@thomsonreuters.com>
2017-12-24 00:14:50 -0500

    change search to multiple process

--- check_spelling.py
+++ check_spelling.py
@@ -1,6 +1,8 @@
 import enchant
 import argparse
 from itertools import permutations
+from multiprocessing import Pool
+import os
 
 
 def check_spelling(word):
@@ -23,6 +25,13 @@ def search(chars):
             print(word)
 
 
+def search_permutation(permutation, length):
+    print('searching for', length, 'char words')
+    print('pid:', os.getpid(), 'parent pid:', os.getppid())
+    for word in search_perm(permutation):
+        print(word)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--chars', help='characters to check spelling against', required=True)
@@ -31,4 +40,10 @@ if __name__ == '__main__':
     if args.chars is not None:
         print('searching for {0}'.format(args.chars))
 
-    search(args.chars)
+    length = len(args.chars) + 1
+    p = Pool(length - 3)
+    for i in range(3, length):
+        p.apply_async(search_permutation, args=([''.join(p) for p in permutations(args.chars, i)], i))
+    p.close()
+    p.join()
+
